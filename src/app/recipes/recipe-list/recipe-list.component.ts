f4cc362003bf2203f386234cb1b5b9df98548403
Patrick Dawson <patrick.soboljew@gmail.com>
2016-08-20 17:18:52 +0200

    Finished first course (section 3).

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from "../recipe";
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: 'recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipe = new Recipe('Dummy', 'Dummy', 'https://upload.wikimedia.org/wikipedia/commons/4/49/Testing22222.jpg');
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+
+}
